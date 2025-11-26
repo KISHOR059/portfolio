d28eb29f4c99b71dc6fbd21c81b0b5d6ea8ec956
KISHOR059 <mtkishor07@gmail.com>
2025-11-26 15:26:14 +0530

    Dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", //  for dark mode to work
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
